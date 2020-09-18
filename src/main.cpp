75b03d0de4e8dcb220ef4ed1acf740b5d70b65bb
Stephanie Vargas <vstephanie774@gmail.com>
2020-09-17 23:02:11 -0600

    Errores solucionados

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Steph on 17/9/2020.
+//
+
+#include<iostream>
+
+int main()
+{
+
+
+
+
+
+
+    return 0;
+}
+
---
+++ tst/Main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Steph on 31/8/2020.
+//
+#include<iostream>
+
+int main(){
+
+
+
+    
+
+    return 0;
+}
